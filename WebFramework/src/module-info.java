7d2b4a6a22b395b8f3aa6b6caf8b6eff5990915c
Fabien CAYRE <fabiencayre81@gmail.com>
2021-08-17 11:10:57 +0200

    Bugfix JUnit

--- WebFramework/src/module-info.java
+++ WebFramework/src/module-info.java
@@ -7,9 +7,6 @@ module webframework {
 	requires toml4j;
 	
 	// Unit testing
-	requires org.junit.jupiter;
-	requires org.junit.jupiter.api;
 	//requires org.junit.jupiter.engine;
-	requires org.junit.jupiter.params;
 	
 }
