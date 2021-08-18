fd7f51fce8bf29bab7f2fc40155b01d64a214b1c
Fabien CAYRE <fabiencayre81@gmail.com>
2021-08-18 10:07:06 +0200

    Added class for debugging in real time

---
+++ WebFramework/src/net/omny/utils/Debug.java
@@ -0,0 +1,31 @@
+package net.omny.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Debug {
+
+	public static final boolean ENABLE = true;
+	
+	private static Map<String, Long> chronos = new HashMap<>();
+	
+	public static void debug(String string) {
+		if(ENABLE)
+			System.out.println("[Debug] "+string);
+	}
+	
+	public static void reset(String tag) {
+		if(ENABLE) {
+			chronos.put(tag, System.currentTimeMillis());
+		}
+	}
+	
+	public static void time(String tag) {
+		if(ENABLE) {
+			if(chronos.containsKey(tag)) {
+				System.out.println("[Timer Debug] Elapsed "+(System.currentTimeMillis()-chronos.get(tag))+" ms from the last time set {"+tag+"}");				
+			}
+		}
+	}
+	
+}
