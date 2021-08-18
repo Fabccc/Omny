fac63544cfcd711be7a288f8100e72b7ea9cd893
Fabien CAYRE <fabiencayre81@gmail.com>
2021-08-18 10:11:08 +0200

    Finished tests

--- WebFramework/src/net/omny/test/Test.java
+++ WebFramework/src/net/omny/test/Test.java
@@ -1,18 +1,26 @@
 package net.omny.test;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import net.omny.route.Router;
 import net.omny.server.WebServer;
 
 public class Test extends WebServer{
 
+	private static final ExecutorService SERVICE = Executors.newScheduledThreadPool(4);
+	
 	public static void main(String[] args) {
 		launch(new Test());
 	}
 	
+	public Test() {
+		super("conf.toml", SERVICE);
+	}
+	
 	@Override
 	public void route(Router router) {
-		// TODO test if we can route some index.html file or even test.html
-		
+		router.route("/", "index.html");
 	}
 
 }
