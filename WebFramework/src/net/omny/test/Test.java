764a50293f9ab891101d6756158b293179c37a5c
Fabien CAYRE <fabiencayre81@gmail.com>
2021-08-22 11:26:11 +0200

    Added more testing routes

--- WebFramework/src/net/omny/test/Test.java
+++ WebFramework/src/net/omny/test/Test.java
@@ -21,7 +21,12 @@ public class Test extends WebServer{
 	@Override
 	public void route(Router router) {
 		router.route("/", "index.html");
-		router.route("/admission", "admission.pdf");
+		router.route("/pdf", "admission.pdf");
+		router.route("/css", "main.css");
+		router.route("/js", "main.js");
+		router.route("/json", "test.json");
+		router.route("/csv", "test.csv");
+		router.route("/test.exe", "test.exe");
 	}
 
 }
