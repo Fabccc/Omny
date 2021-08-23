7d1135203901c5186d17f822d5f2cd55fcb3a864
Fabien CAYRE <fabiencayre81@gmail.com>
2021-08-23 20:09:41 +0200

    Per thread debug chronos

--- WebFramework/src/net/omny/utils/Debug.java
+++ WebFramework/src/net/omny/utils/Debug.java
@@ -6,32 +6,40 @@ import java.util.Map;
 public class Debug {
 
 	public static final boolean ENABLE = true;
-	
-	private static Map<String, Long> chronos = new HashMap<>();
-	
+
+	private static ThreadLocal<Map<String, Long>> chronos = new ThreadLocal<>() {
+		@Override
+		public java.util.Map<String, Long> initialValue() {
+			return new HashMap<>();
+		};
+	};
+
 	public static void debug(String string) {
-		if(ENABLE)
-			System.out.println("[Debug] "+string);
+		if (ENABLE)
+			System.out.println("[Debug] " + string);
 	}
-	
+
 	public static void reset(String tag) {
-		if(ENABLE) {
-			chronos.put(tag, System.currentTimeMillis());
+		if (ENABLE) {
+			chronos.get().put(tag, System.currentTimeMillis());
 		}
 	}
-	
+
 	public static void warn(String string) {
-		if(ENABLE) {
-			System.err.println("[Warn Debug] "+string);
+		if (ENABLE) {
+			System.err.println("[Warn Debug] " + string);
 		}
 	}
-	
+
 	public static void time(String tag) {
-		if(ENABLE) {
-			if(chronos.containsKey(tag)) {
-				System.out.println("[Timer Debug] Elapsed "+(System.currentTimeMillis()-chronos.get(tag))+" ms from the last time set {"+tag+"}");				
+		if (ENABLE) {
+			if (chronos.get().containsKey(tag)) {
+				System.out
+					.println(
+						"[Timer Debug] Elapsed " + (System.currentTimeMillis() - chronos.get().get(tag)) + " ms from the last time set {"
+							+ tag + "}");
 			}
 		}
 	}
-	
+
 }
