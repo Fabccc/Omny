9f0175fe46d23db57b6f9134cfbbcc554fe1ba8a
Fabien <fabiencayre81@gmail.com>
2021-08-27 08:13:17 +0200

    Begining JsonView

--- WebFramework/src/net/omny/route/impl/JsonRoute.java
+++ WebFramework/src/net/omny/route/impl/JsonRoute.java
@@ -1,5 +1,24 @@
 package net.omny.route.impl;
 
-public class JsonRoute {
-  
+import lombok.Getter;
+import net.omny.route.Request;
+import net.omny.route.Response;
+import net.omny.route.Route;
+import net.omny.views.JsonView;
+import net.omny.views.View;
+
+public class JsonRoute implements Route{
+
+  @Getter
+  private JsonView view;
+
+  private JsonRoute(){
+    this.view = new JsonView();
+  }
+
+  @Override
+  public View handle(Request req, Response res) {
+    res.setHeader("Content-Header", "application/json");
+    return this.view;
+  }
 }
---
+++ WebFramework/src/net/omny/views/JsonView.java
@@ -0,0 +1,13 @@
+package net.omny.views;
+
+import net.omny.route.Response;
+
+public class JsonView implements View{
+
+  @Override
+  public void write(Response res) {
+    // TODO Auto-generated method stub
+    res.addBody("");
+  }
+  
+}
