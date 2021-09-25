d41c455743194333bd98980c94b737b1b245cd17
Fabien <fabiencayre81@gmail.com>
2021-09-25 17:58:38 +0200

    Begin of paramUrlHandler

---
+++ WebFramework/src/net/omny/route/handlers/ParamURLHandler.java
@@ -0,0 +1,17 @@
+package net.omny.route.handlers;
+
+import java.io.IOException;
+import java.net.Socket;
+
+import net.omny.route.Request;
+import net.omny.route.Router;
+
+public class ParamURLHandler implements RequestHandler{
+
+  @Override
+  public boolean handle(Router router, Request request, Socket client) throws IOException {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+}
