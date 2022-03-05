157e08c6ddfd0357584ca063a9f3b139120f0c7c
Fabien CAYRE <fabiencayre81@gmail.com>
2022-03-05 14:29:16 +0100

    Added namespace routing

--- webengine/src/net/omny/test/Test.java
+++ webengine/src/net/omny/test/Test.java
@@ -3,7 +3,10 @@ package net.omny.test;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
+import com.google.gson.Gson;
+
 import net.omny.route.HTTP;
+import net.omny.route.Method;
 import net.omny.route.NamedRouter;
 import net.omny.route.Request;
 import net.omny.route.Response;
@@ -11,33 +14,35 @@ import net.omny.route.Route;
 import net.omny.route.Router;
 import net.omny.route.RouterOptions;
 import net.omny.route.impl.FileRoute;
+import net.omny.route.impl.JsonRoute;
 import net.omny.route.impl.TextRoute;
 import net.omny.server.WebServer;
 import net.omny.utils.Debug;
 import net.omny.views.TextView;
 import net.omny.views.View;
 
-public class Test extends WebServer{
-
+public class Test extends WebServer {
 
 	/*
-	Brian Goetz in his famous book "Java Concurrency in Practice" recommends the following formula:
-
-	Number of threads = Number of Available Cores * (1 + Wait time / Service time)
-
-	*/
+	 * Brian Goetz in his famous book "Java Concurrency in Practice" recommends the
+	 * following formula:
+	 * 
+	 * Number of threads = Number of Available Cores * (1 + Wait time / Service
+	 * time)
+	 * 
+	 */
 	private static final int THREAD_COUNT = 12;
 	private static final ScheduledExecutorService SERVICE = Executors.newScheduledThreadPool(THREAD_COUNT);
-	
+
 	public static void main(String[] args) {
 		Debug.ENABLE = true;
 		launch(new Test());
 	}
-	
+
 	public Test() {
 		super("conf.toml", SERVICE, THREAD_COUNT);
 	}
-	
+
 	@Override
 	public void route(Router router) {
 		router.route(TestRouter.class);
@@ -45,56 +50,60 @@ public class Test extends WebServer{
 		router.route(new NestedRouter1());
 		router.staticRoute("./webengine/static");
 	}
-	
+
 	public static class TestRouter {
-		
+
 		@HTTP(url = "/")
 		public Route indexRoute = new FileRoute("webengine/index.html");
 
 		@HTTP(url = "/loulou")
 		public Route fileRoute = new FileRoute("webengine/loulou.json");
-		
+
 		@HTTP(url = "/trolol")
 		public View index(Request req, Response res) {
 			res.setHeader("Content-Type", "text/plain");
 			return new TextView("trolololllololololololololololo");
 		}
-		
+
 	}
 
 	@RouterOptions(namespace = "/test")
-	public static class NamespaceTestRouter{
+	public static class NamespaceTestRouter {
 
 		@HTTP(url = "user")
 		public Route userApi = new TextRoute("Oula bizarrrreee");
 
 	}
 
-	public static class NestedRouter1 extends NamedRouter{
+	public static class NestedRouter1 extends NamedRouter {
 
 		public NestedRouter1() {
 			super("api");
 		}
 
 		@Override
-		public Router route(Router router) {
-			// TODO Auto-generated method stub
-			return super.route(router);
+		public void route() {
+			route(new NestedRouter2());
 		}
 
 	}
 
-	public static class NestedRouter2 extends NamedRouter{
+	public static class NestedRouter2 extends NamedRouter {
 
 		public NestedRouter2() {
 			super("user");
 		}
 
 		@Override
-		public Router route(Router router) {
-			// TODO Auto-generated method stub
-			return super.route(router);
+		public void route() {
+			route("/name", new JsonRoute(new Gson().toJson(new UserJson())), Method.GET);
 		}
+
+	}
+
+	public static class UserJson {
+
+		public String name = "Sammy";
 
 	}
 
