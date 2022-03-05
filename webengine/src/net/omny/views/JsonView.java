fc35d555cd759f730bde9469aa7010932bb4a91e
Fabien CAYRE <fabiencayre81@gmail.com>
2022-03-05 14:28:40 +0100

    Added JsonView from String

--- webengine/src/net/omny/views/JsonView.java
+++ webengine/src/net/omny/views/JsonView.java
@@ -6,15 +6,19 @@ import net.omny.route.Response;
 
 public class JsonView implements View{
 
-  private Gson gson;
+  private String gson;
 
   public JsonView(Gson gson){
-    this.gson = gson;
+    this.gson = gson.toString();
+  }
+
+  public JsonView(String string) {
+    this.gson = string;
   }
 
   @Override
   public void write(Response res) {
-    res.addBody(gson.toString());
+    res.addBody(gson);
   }
   
 }
