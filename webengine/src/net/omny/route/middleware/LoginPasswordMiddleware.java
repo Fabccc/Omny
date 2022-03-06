3e2913c001305cc1ab2b7e335fff13ef09bce3d3
FabienCAYRE <fabiencayre81@gmail.com>
2022-03-06 18:01:33 +0100

    Added authentification middleware with login password

---
+++ webengine/src/net/omny/route/middleware/LoginPasswordMiddleware.java
@@ -0,0 +1,24 @@
+package net.omny.route.middleware;
+
+import net.omny.route.Request;
+import net.omny.utils.HTTPUtils.Headers;
+
+public abstract class LoginPasswordMiddleware extends AuthentificationMiddleware{
+
+    public LoginPasswordMiddleware(String url) {
+        super(url);
+    }
+
+    @Override
+    public boolean auth(Request request) {
+        String auth = request.getHeader(Headers.AUTHORIZATION);
+        if(!auth.startsWith("Basic ")){
+            return false;
+        }
+        // TODO
+        return false;
+    }
+
+    public abstract boolean auth(String login, String password);
+    
+}
