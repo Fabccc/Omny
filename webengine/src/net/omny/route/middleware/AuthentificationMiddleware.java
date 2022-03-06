9bb9fe41d61a6df1919a8a7eda3d51cc155fb5e3
FabienCAYRE <fabiencayre81@gmail.com>
2022-03-06 18:01:20 +0100

    Added authentification middleware

--- webengine/src/net/omny/route/middleware/AuthentificationMiddleware.java
+++ webengine/src/net/omny/route/middleware/AuthentificationMiddleware.java
@@ -6,21 +6,25 @@ import java.net.Socket;
 import net.omny.route.Request;
 import net.omny.route.Router;
 import net.omny.server.WebServer;
+import net.omny.utils.HTTPUtils.Headers;
 
-public abstract class AuthentificationMiddleware implements Middleware{
+public abstract class AuthentificationMiddleware implements Middleware {
 
   private String url;
 
-  public AuthentificationMiddleware(String url){
+  public AuthentificationMiddleware(String url) {
     this.url = url;
   }
 
   @Override
   public boolean handle(WebServer webServer, Router router, Request request, Socket client) throws IOException {
-    // TODO Auto-generated method stub
-    return false;
+    if (request.equalsPath(url, true))
+      return false;
+    if (!request.containsHeader(Headers.AUTHORIZATION))
+      return false;
+    return auth(request);
   }
-  
 
+  public abstract boolean auth(Request request);
 
 }
