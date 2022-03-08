e7097aa149251a0834ee3abc756d11a7a3972950
FabienCAYRE <fabiencayre81@gmail.com>
2022-03-08 10:39:55 +0100

    Testing parameters

--- webengine/src/net/omny/test/Test.java
+++ webengine/src/net/omny/test/Test.java
@@ -4,6 +4,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import net.omny.route.Code;
 import net.omny.route.HTTP;
 import net.omny.route.Method;
 import net.omny.route.NamedRouter;
@@ -17,6 +18,8 @@ import net.omny.route.impl.JsonRoute;
 import net.omny.route.impl.TextRoute;
 import net.omny.server.WebServer;
 import net.omny.utils.Debug;
+import net.omny.utils.HTTPUtils;
+import net.omny.utils.HTTPUtils.MimeType;
 import net.omny.views.JsonView;
 import net.omny.views.TextView;
 import net.omny.views.View;
@@ -97,6 +100,38 @@ public class Test extends WebServer {
 		@Override
 		public void route() {
 			route("/name", new ApiUserNameRoute(), Method.GET);
+			route(new ApiUserProfileRoute());
+		}
+
+	}
+
+	public static class ApiUserProfileRoute extends JsonRoute{
+
+		public ApiUserProfileRoute() {
+			setAllowCache(false);
+			setPath("/name/:uuid");
+			setMethod(Method.GET);
+		}
+
+		@Override
+		public View handle(Request req, Response res) {
+			super.handle(req, res);
+			String s = req.getParams("uuid");
+			if(s == null){
+				return new HTTPUtils.ErrorView(Code.E400_BAD_REQUEST, 
+				"""
+					{
+						"error": "Missing parameter"
+					}	
+				""", MimeType.JSON);
+			}
+
+			return new JsonView("""
+					{
+						"uuid": "%s",
+						"name": "%s"
+					}
+					""".formatted(s, "LeTroll :pingching_hand:"));
 		}
 
 	}
@@ -106,7 +141,6 @@ public class Test extends WebServer {
 		private AtomicInteger test;
 
 		public ApiUserNameRoute() {
-			super("");
 			this.test = new AtomicInteger();
 			this.setAllowCache(true);
 			this.setLastInCache(100);
