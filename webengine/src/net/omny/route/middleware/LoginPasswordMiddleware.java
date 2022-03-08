727dacdb77e227f64a27581abc3091068b95e4f7
FabienCAYRE <fabiencayre81@gmail.com>
2022-03-08 16:58:46 +0100

    Added LoginPasswordMiddleware

--- webengine/src/net/omny/route/middleware/LoginPasswordMiddleware.java
+++ webengine/src/net/omny/route/middleware/LoginPasswordMiddleware.java
@@ -1,5 +1,7 @@
 package net.omny.route.middleware;
 
+import org.codehaus.plexus.util.Base64;
+
 import net.omny.route.Request;
 import net.omny.utils.HTTPUtils.Headers;
 
@@ -19,8 +21,11 @@ public abstract class LoginPasswordMiddleware extends AuthentificationMiddleware
         if(!auth.startsWith("Basic ")){
             return false;
         }
-        // TODO
-        return false;
+        String base64 = auth.substring("Basic ".length());
+        String[] credentials = new String(Base64.decodeBase64(base64.getBytes())).split("\\:");
+        String login = credentials[0];
+        String password = credentials[1];
+        return auth(login, password);
     }
 
     public abstract boolean auth(String login, String password);
